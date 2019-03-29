f1c081ead61c7215fbd093eb6fa94b993642bfb2
Felix Schütze <dev@felix-schuetze.de>
2019-03-29 16:35:05 +0100

    Update tictactoe.sh

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 a="1|2|3\n4|5|6\n7|8|9"
-s=x
+s=xo
 while [ -z "${a##*[0-9]*}" ];do
 echo -e $a
-read d
-b="${a/$d/$s}"
-if ! [ $a == $b ]
-then
-a=$b
-s=$(echo $s|tr xo ox)
-fi
+read -p "${s:$u:1}> " d
+b="${a/$d/${s:$u:1}}"
+[ $a != $b ]&&(((u^=1)<2))&&a=$b
 done
 echo -e $a
